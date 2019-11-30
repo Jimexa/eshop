d7578ef2b8f64e0f459fa5284c9409622f71c2d7
Dimitris Exadaktylos <jimexa@yahoo.gr>
2019-11-30 22:19:59 +0200

    add mobx-persist

--- src/components/store/shopStore.js
+++ src/components/store/shopStore.js
@@ -1,4 +1,5 @@
 import { observable, decorate, autorun } from "mobx";
+import { persist, create } from "mobx-persist";
 
 class ShopStore {
   value1 = 1;
@@ -6,16 +7,15 @@ class ShopStore {
   itemList = [];
 }
 decorate(ShopStore, {
-  value1: observable,
+  value1: [persist("object"), observable],
   total: observable,
   itemList: observable
 });
 
-let store = (window.store = new ShopStore());
+const hydrate = create({ storage: localStorage, jsonify: false });
+
+const store = (window.store = new ShopStore());
 
 export default store;
 
-// autorun(() => {
-//   console.log(store.value1);
-//   console.log(store.total);
-// });
+hydrate("shopStore", store).then(() => console.log("store has been hydrated"));
